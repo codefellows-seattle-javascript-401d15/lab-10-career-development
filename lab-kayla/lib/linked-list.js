f736553eaed50041763d7083e2421fb778801cb3
Kayla Asay <kdasay16@gmail.com>
2017-05-01 12:44:13 -0700

    not done

---
+++ lab-kayla/lib/linked-list.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const Node = require('./reverse');
+
+const SLL = module.exports = function() {
+  this.head = null;
+};
+
+SLL.prototype.prepend = function(value) {
+  let node = new Node(value);
+  if(!this.head) {
+    this.head = node;
+    return this;
+  }
+
+  node.next = this.head;
+  this.head = node;
+  return this;
+};
+
+SLL.prototype.append = function(value) {
+  let node = new Node(value);
+  let lastNode = null;
+
+  if(!this.head) {
+    this.head = node;
+    return this;
+  }
+
+  _setLastNode(this.head);
+  lastNode.next = node;
+  return this;
+
+  function _setLastNode(node) {
+    if(!node) return;
+    lastNode = node;
+    _setLastNode(node.next);
+  }
+};
--- lab-kayla/lib/reverse.js
+++ lab-kayla/lib/reverse.js
@@ -1,30 +1,37 @@
 'use strict';
 
-function reverse(list) {
-  let currentNode = this.head.next;
-  if(!lastNode) {
-    let originalHead = this.head
-  } else {
-     currentNode.next = lastNode;
-   };
-  let lastNode = _findLastNode(list);
-  if(lastNode.next === null) {
-    lastNode.next = originalHead.next;
-  }
-  if(originalHead === lastNode){
-    return list;
-  };
-  nodeFinder(list)
-  reverse(list);
+module.exports = function(value) {
+  this.val = value;
+  this.next = null;
 };
+//I am refactoring this
 
- let nodeFinder = function _findLastNode(list) {
-   let returnedNode;
-   let currentNode = this.head;
-   if(currentNode.next === null) {
-     returnedNode = currentNode;
-     return;
-   }
-   let this.head = currentNode.next;
-   nodeFinder(currentNode);
- };
+// function reverse(value) {
+//   let currentNode = this.head.next;
+//   if(!lastNode) {
+//     let originalHead = this.head;
+//   } else {
+//     currentNode.next = lastNode;
+//   }
+//   let lastNode = nodeFinder(value);
+//   if(lastNode.next === null) {
+//     lastNode.next = originalHead.next;
+//   }
+//   if(originalHead === lastNode){
+//     return value;
+//   }
+//
+//   let nodeFinder = function() {
+//     let returnedNode;
+//     let currentNode = this.head;
+//     if(currentNode.next === null) {
+//       returnedNode = currentNode;
+//       return;
+//     }
+//     this.head = currentNode.next;
+//     nodeFinder(currentNode);
+//   };
+//
+//   nodeFinder(value);
+//   reverse(value);
+// }
