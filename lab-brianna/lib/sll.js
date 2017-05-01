2819f2a05bb9f3f2a1d9ff44e06a2a4fa273e5f7
Brianna <briannaburrows@gmail.com>
2017-05-01 11:44:40 -0700

    completed reverse function

---
+++ lab-brianna/lib/sll.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const Node = require('./list-node');
+
+const SLL = module.exports = function () {
+  this.head = null;
+};
+
+SLL.prototype.prepend = function(value) {
+  let node = new Node(value);
+  if(!this.head) {
+    this.head = node;
+    return this;
+  }
+  node.next = this.head;
+  this.head = node;
+  return this;
+};
+
+SLL.prototype.append = function(value) {
+  let node = new Node(value);
+  let lastNode = null;
+
+  if(!this.head) {
+    this.head = node;
+    return;
+  }
+
+  _setLastNode(this.head);
+  lastNode.next = node;
+  return this;
+
+
+  function _setLastNode(node) {
+    if(!node) return;
+    lastNode = node;
+    _setLastNode(node.next);
+  }
+};
+
+SLL.prototype.remove = function(){
+
+};
+
+SLL.prototype.reverse = function() {
+  if(!this.head || !this.head.next) return; {
+    let placeOne = null;
+    let placeTwo = this.head;
+    let placeThree;
+  }
+  while (placeTwo) {
+    placeThree = placeTwo.next;
+    placeTwo.next = placeOne;
+    placeOne = placeTwo;
+    placeTwo = placeThree;
+  }
+  this.head = placeOne;
+};
---
+++ lab-brianna/tests/tests.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const List = require('../lib/sll');
+const chai = require('chai');
+
+//create a list (origList) and append values. Then test that origList is correct. Then test reverse method and compare that origList and new List are not equal to each other.
+describe('test reverse method on single list link', function () {
+  describe('#reverse', function () {
+    let origList = new SLL;
+    it('should ');
+  });
+});
